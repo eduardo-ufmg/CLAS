0754ae4936611bbda1357ba329d925f30f08355a
eduardo-ufmg <eduardohbc@ufmg.br>
2025-02-23 09:18:39 -0300

    fix make

--- makeall.sh
+++ makeall.sh
@@ -3,18 +3,24 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Function to recursively find directories with a Makefile and run make clean and make
+# Check if an argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <all|clean>"
+  exit 1
+fi
+
+# Function to recursively find directories with a Makefile and run make or make clean
 process_directories() {
   for dir in "$1"/*; do
     if [ -d "$dir" ]; then
       if [ -f "$dir/Makefile" ]; then
         echo "Processing $dir"
-        (cd "$dir" && make)
+        (cd "$dir" && make $2)
       fi
-      process_directories "$dir"
+      process_directories "$dir" "$2"
     fi
   done
 }
 
 # Start processing from the current directory
-process_directories "$(pwd)"
+process_directories "$(pwd)" "$1"
