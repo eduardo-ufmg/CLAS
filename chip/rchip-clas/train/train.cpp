8d9a6fac039c66f77ef422232212b201c502e1fb
eduardo-ufmg <eduardohbc@ufmg.br>
2025-03-19 09:27:19 -0300

    feat: add computeSVs and computeExperts functions, update CMake configurations, and refactor labeling logic

---
+++ chip/rchip-clas/train/train.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+#include "types.hpp"
+#include "filenameHelpers.hpp"
+#include "readFiles.hpp"
+#include "chipcid.hpp"
+#include "gabrielGraph.hpp"
+#include "filter.hpp"
+#include "computeExperts.hpp"
+#include "writeFiles.hpp"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+  float tolerance = ns_filter::DEFAULT_TOLERANCE;
+
+  if (argc < 2) {
+    cerr << "Usage: " << argv[0] << " <dataset> [tolerance]" << endl;
+  }
+
+  if (argc > 2) {
+    tolerance = stof(argv[2]);
+  }
+
+  const string dataset_file_path = argv[1];
+
+  Vertices vertices = readDataset(dataset_file_path);
+
+  computeGabrielGraph(vertices);
+
+  filter(vertices, tolerance);
+
+  const Experts experts = computeExperts(vertices);
+
+  const chipIDbimap chipidbimap = getchipIDmap(vertices, experts);
+
+  const string output_file_path = "./train/chips-" + filenameFromPath(dataset_file_path);
+  const string chipidmap_file_path = "./train/chipidbimap-" + filenameFromPath(dataset_file_path);
+
+  if (writeExperts(experts, output_file_path) != 0) {
+    cerr << "Error: could not write experts to file" << output_file_path << endl;
+    return 1;
+  }
+
+  if (writechipIDmap(chipidbimap, chipidmap_file_path) != 0) {
+    cerr << "Error: could not write chipidbimap to file" << chipidmap_file_path << endl;
+    return 1;
+  }
+
+}
